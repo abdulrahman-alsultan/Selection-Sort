35188aacd66ffbc399e1067b1d2135863126388d
abdulrahman-alsultan <56443428+abdulrahman-alsultan@users.noreply.github.com>
2021-06-17 08:40:39 +0300

    Add files via upload

---
+++ SelctionSort.py
@@ -0,0 +1,13 @@
+def selctionSort(list):
+    for i in range(len(list)):
+        current_minimum = i
+        for j in range(i + 1, len(list)):
+            if list[current_minimum] > list[j]:
+                current_minimum = j
+        list[i], list[current_minimum] = list[current_minimum], list[i]
+
+
+A = [64, 25, 12, 22, 11]
+b = [20, 12, 10, 15, 2]
+selctionSort(b)
+print(b)
